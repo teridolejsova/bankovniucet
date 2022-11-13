e0c55aa543d6a5f6b59a7faaa7231f631182db2a
Tereza Dolejšová <terdoe>
2022-11-13 19:20:49 +0100

    New Spring project

---
+++ src/main/java/cz/tera/bankovniucet/BankovniUcetApplication.java
@@ -0,0 +1,13 @@
+package cz.tera.bankovniucet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankovniUcetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankovniUcetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/tera/bankovniucet/BankovniUcetApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.tera.bankovniucet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankovniUcetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
